8983e23a9612b9a29fa3e4382cc995e60a5e46b0
Drumato <drumato43@gmail.com>
2022-06-05 08:20:13 +0000

    feat(style): setup favicons
    Signed-off-by: Drumato <drumato43@gmail.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,56 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Link from "next/link";
 
+const GoogleFontDOM = (): JSX.Element => {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+    </>
+  );
+};
+const FaviconDOM = (): JSX.Element => {
+  return (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicons/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicons/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
+    </>
+  );
+};
+
 const MyDocument = () => {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <GoogleFontDOM />
         <link
           href="https://fonts.googleapis.com/css2?family=Klee+One&family=M+PLUS+1p&family=Noto+Sans+JP&display=swap"
           rel="stylesheet"
-        ></link>
-        <></>
+        />
+        <FaviconDOM />
       </Head>
       <body>
         <Main />
@@ -20,4 +59,5 @@ const MyDocument = () => {
     </Html>
   );
 };
+
 export default MyDocument;
