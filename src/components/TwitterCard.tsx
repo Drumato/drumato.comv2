fdc7638e100afaabf28e05c6e76a3fc8a598ab02
Drumato <drumato43@gmail.com>
2022-06-07 23:48:41 +0000

    fix: add @
    Signed-off-by: Drumato <drumato43@gmail.com>

--- src/components/TwitterCard.tsx
+++ src/components/TwitterCard.tsx
@@ -11,8 +11,8 @@ const TwitterCard = (props: TwitterCardProps): JSX.Element => {
   return (
     <Head>
       <meta name="twitter:card" content={props.cardContent} />
-      <meta name="twitter:site" content={`${props.userID}`} />
-      <meta name="twitter:creator" content={`${props.userID}`} />
+      <meta name="twitter:site" content={`@${props.userID}`} />
+      <meta name="twitter:creator" content={`@${props.userID}`} />
       <meta name="twitter:title" content={props.title} />
       <meta name="twitter:description" content={props.description} />
     </Head>
