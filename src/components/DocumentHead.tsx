7f2563f55eaa0700c590d2684edb13658656fc5e
Drumato <drumato43@gmail.com>
2022-06-07 03:31:57 +0000

    refactor: separate favicons
    Signed-off-by: Drumato <drumato43@gmail.com>

--- src/components/DocumentHead.tsx
+++ src/components/DocumentHead.tsx
@@ -1,8 +1,9 @@
 import Head from "next/head";
+import Favicons from "./Favicons";
 
 type Props = {};
 
-const GoogleFontDOM = (): JSX.Element => {
+const GoogleFont = (): JSX.Element => {
   return (
     <>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -11,44 +12,12 @@ const GoogleFontDOM = (): JSX.Element => {
   );
 };
 
-const FaviconDOM = (): JSX.Element => {
-  return (
-    <>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicons/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicons/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicons/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/favicons/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicons/safari-pinned-tab.svg"
-        color="#5bbad5"
-      />
-      <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#ffffff" />
-    </>
-  );
-};
-
 const DocumentHead = (_props: Props): JSX.Element => {
   return (
     <Head>
       <title>drumato.com</title>
-      <GoogleFontDOM />
-      <FaviconDOM />
+      <GoogleFont />
+      <Favicons />
     </Head>
   );
 };
